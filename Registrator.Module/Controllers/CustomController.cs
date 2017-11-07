7a7d30af923cfa0aa7170b0ebde81e704ddcf3d5
Aldar Ikihnov <aldarcose@inbox.ru>
2017-11-07 14:25:34 +0800

    Prevent SimultaneousDataModificationException

---
+++ Registrator.Module/Controllers/CustomController.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.SystemModule;
+using System;
+
+namespace Registrator.Module.Controllers
+{
+    /// <summary>
+    /// Prevent SimultaneousDataModificationException raised when modified object is 
+    /// opened from nestedListView in new MDI Tab
+    /// </summary>
+    public partial class CustomController : ViewController
+    {
+        public CustomController()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            Frame.GetController<ListViewProcessCurrentObjectController>().CustomProcessSelectedItem += 
+                new EventHandler<CustomProcessListViewSelectedItemEventArgs>(object_CustomProcessSelectedItem);
+        }
+
+        void object_CustomProcessSelectedItem(object sender, CustomProcessListViewSelectedItemEventArgs e)
+        {
+            if (View.ObjectSpace.ModifiedObjects.Contains(e.InnerArgs.CurrentObject))
+            {
+                View.ObjectSpace.CommitChanges();
+            }
+        }
+    }
+}
